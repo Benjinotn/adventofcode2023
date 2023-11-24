ec83c3801611f731606e5e0541e69df104580513
Ben Norton <benjaminnorton1@outlook.com>
2023-11-24 14:29:38 +0000

    added directories and python placeholders

--- setup/setup.py
+++ setup/setup.py
@@ -6,8 +6,22 @@ os.chdir(root_dir)
 
 for day in range(1, 26):
     dir_to_add = root_dir + "\day" + str(day)
-
+    file_to_add = dir_to_add + "\day" + str(day) + ".py"
+    print(file_to_add)
     print(dir_to_add)
 
-    os.mkdir(dir_to_add)
+    if not os.path.exists(dir_to_add):
+        print(dir_to_add , "Created")
+        os.mkdir(dir_to_add)
+    else:
+        print("Folder already exists, skipping creation")
+
+    try:
+        fp = open(file_to_add, 'x')
+        fp.close()
+    except FileExistsError:
+        print("File already exists, skipping creation")
+        
+
+
 
